50ff1e95369be803546a7d983a8f6002eba3312f
NRMRaiyan <nrmraiyan@gmail.com>
2026-05-22 01:17:16 +0600

    add flightBooker

---
+++ flightBooker.java
@@ -0,0 +1,13 @@
+import javax.swing.x;
+import java.awt.*;
+import java.awt.vent;
+
+public class flightBooker{
+    public flightBooker(){
+
+    }
+
+    public static void main(String [] args){
+        new flightBooker();
+    }
+}
